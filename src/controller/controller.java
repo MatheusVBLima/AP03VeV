c58472ed02a6f2e98f2fd51c13c591142cde85a8
NatanLucena <44265910+NatanLucena@users.noreply.github.com>
2022-06-29 20:33:50 -0300

    Comentários adicionados

---
+++ src/controller/controller.java
@@ -0,0 +1,92 @@
+package AP03.src.controller;
+import AP03.src.model.Aluno;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map
+
+public class controller {
+
+    private LinkedHashMap<String, Aluno> usuarios = new LinkedHashMap<String, Aluno>();
+
+    public void put(String matricula, String nome) {
+        if(maxAlunos){
+            System.out.println("Nao e possivel adicionar mais alunos");
+        }else {
+            aluno = new Aluno(matricula, nome);
+		    usuarios.put(matricula, aluno); 
+        }
+	}
+
+    public Aluno get(String matricula) {
+        return usuarios.get(matricula);
+    }
+
+    public boolean containsAluno(String matricula) {
+        return usuarios.containsKey(matricula);
+    }
+
+    public int size() {
+        return usuarios.size();
+    }
+
+    public void removeAluno(String matricula) {
+        usuarios.remove(matricula);
+    }
+
+    public float getMediaParcial(String matricula) {
+        return usuarios.get(matricula).getMediaParcial();
+    }
+
+    public String setNota1(String matricula, float nota) {
+        if( notaValida(nota) ) {
+            usuarios.get(matricula).setNota1(nota);
+            return "Nota 1 alterada com sucesso!";
+        }else {
+            return ("Nota invalida");
+        }
+    }
+
+    public String setNota2(String matricula, float nota) {
+        if( notaValida(nota) ) {
+            usuarios.get(matricula).setNota2(nota);
+            return "Nota 2 alterada com sucesso!";
+        }else {
+            return ("Nota invalida");
+        }
+    }
+
+    public String setNota3(String matricula, float nota) {
+        if( notaValida(nota) ) {
+            aluno = usuarios.get(matricula);
+            aluno.setNota3(nota);
+            aluno.setMedia(aluno.getNota1() + aluno.getNota2() + aluno.getNota3());
+            return "Nota 3 alterada com sucesso!";
+        }else {
+            return ("Nota invalida");
+        }
+    }
+
+    public float getNota1(String matricula) {
+        return usuarios.get(matricula).getNota1();
+    }
+
+    public float getNota2(String matricula) {
+        return usuarios.get(matricula).getNota2();
+    }
+
+    public float getNota3(String matricula) {
+        return usuarios.get(matricula).getNota3();
+    }
+
+    public float getMedia(String matricula) {
+        return usuarios.get(matricula).getMedia();
+    }
+
+    private boolean notaValida(float nota) {
+        return nota >= 0 && nota <= 10;
+    }
+
+    private boolean maxAlunos() {
+        return usuarios.size() >= 5;
+    }
+}
--- src/test/AlunoTest.java
+++ src/test/AlunoTest.java
@@ -12,18 +12,21 @@ import controller.Controller;
 class AlunoTest {
 
 	public Controller controller;
-
+	
+	//Resetando o controller antes de cada teste.
 	@BeforeEach
 	public void setUp() {
 		controller = new Controller();
 	}
 
+	//Teste para verificar se o aluno foi cadastrado com sucesso.
 	@Test
 	public void cadastraAluno() {
 		controller.put("1", "aluno1");
 		assertTrue(controller.containsAluno("1"));
 	}
 
+	//Teste para verificar se mÃºltiplos alunos foram cadastrados.
 	@Test
 	public void sizeAlunos() {
 		controller.put("1", "aluno1");
@@ -31,6 +34,8 @@ class AlunoTest {
 		assertEquals(2, controller.size());
 	}
 
+
+	//Teste para verificar se um aluno Ã© aprovado corretamente.
 	@Test
 	public void alunoAprovado() {
 		controller.put("1", "aluno1");
@@ -40,25 +45,48 @@ class AlunoTest {
 		assertTrue(controller.getMedia("1") >= 7);
 	}
 
-	@Test
-	public void alunoReprovado() {
-		controller.put("1", "aluno1");
-		controller.setNota1("1", 4);
-		controller.setNota2("1", 5);
-		controller.setNota3("1", 6);
-		assertFalse(controller.getMedia("1") >= 7);
-	}
+	//Teste para verificar se um aluno Ã© aprovado com valor limite.
+    @Test
+    public void alunoAprovadoLimite() {
+        controller.put("1", "aluno1");
+        controller.setNota1("1", 7.1);
+        controller.setNota2("1", 7.1);
+        controller.setNota3("1", 7.1);
+        assertTrue(controller.getMedia("1") >= 7);
+    }
 
-	@Test
-	public void maxAlunos() {
-		controller.put("1", "aluno1");
-		controller.put("2", "aluno2");
-		controller.put("3", "aluno3");
-		controller.put("4", "aluno4");
-		controller.put("5", "aluno5");
-		assertEquals(controller.size(), 5);
-	}
+	//Teste para verificar se um aluno Ã© reprovado corretamente.
+    @Test
+    public void alunoReprovado() {
+        controller.put("1", "aluno1");
+        controller.setNota1("1", 4);
+        controller.setNota2("1", 5);
+        controller.setNota3("1", 6);
+        assertFalse(controller.getMedia("1") >= 7);
+    }
 
+	//Teste para verificar se um aluno Ã© reprovado com valor limite.
+    @Test
+    public void alunoReprovadoLimite() {
+        controller.put("1", "aluno1");
+        controller.setNota1("1", 6.9);
+        controller.setNota2("1", 6.9);
+        controller.setNota3("1", 6.9);
+        assertFalse(controller.getMedia("1") >= 7);
+    }
+
+	//Teste para verificar o limite de alunos cadastrados.
+    @Test
+    public void maxAlunos() {
+        controller.put("1", "aluno1");
+        controller.put("2", "aluno2");
+        controller.put("3", "aluno3");
+        controller.put("4", "aluno4");
+        controller.put("5", "aluno5");
+        assertEquals(controller.size(), 5);
+    }
+
+	//Teste para verificar se o limite de alunos cadastrados esta sendo respeitado.
 	@Test
 	public void overAlunos() {
 		controller.put("1", "aluno1");
@@ -66,25 +94,7 @@ class AlunoTest {
 		controller.put("3", "aluno3");
 		controller.put("4", "aluno4");
 		controller.put("5", "aluno5");
-		assertEquals(controller.put("6", "aluno6"), "Não é possível adicionar mais alunos");
-	}
-
-	@Test
-	public void alunoAprovadoLimite() {
-		controller.put("1", "aluno1");
-		controller.setNota1("1", 7.1);
-		controller.setNota2("1", 7.1);
-		controller.setNota3("1", 7.1);
-		assertTrue(controller.getMedia("1") >= 7);
-	}
-
-	@Test
-	public void alunoReprovadoLimite() {
-		controller.put("1", "aluno1");
-		controller.setNota1("1", 6.9);
-		controller.setNota2("1", 6.9);
-		controller.setNota3("1", 6.9);
-		assertFalse(controller.getMedia("1") >= 7);
+		assertEquals(controller.put("6", "aluno6"), "Nao e possivel adicionar mais alunos");
 	}
 
 }
